9488dc6b538b271eacc37c50835a6c726a54c067
Max Larsson <maxslarsson@gmail.com>
2022-10-14 11:00:45 -0400

    Add form processing to add tweets

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function processForm(e) {
+    e.preventDefault();
+    console.log(e);
+
+    // store the thing that was typed
+    const tweet = document.getElementById("tweet");
+
+    // return false if the tweet is empty
+    if (tweet.value === '') return false;
+
+    // show the tweet in our timeline
+    const ul = document.getElementById("timeline");
+    const li = document.createElement('li');
+    li.innerText = tweet.value;
+    ul.appendChild(li);
+
+    // clear the input box
+    tweet.value = '';
+
+    return false;
+}
+
+const form = document.getElementById("send-tweet");
+form.addEventListener("submit", processForm);
